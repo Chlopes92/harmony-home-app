f67d213bdf68ade31938d431db28dd65adccb223
Christelle <christelle.lopes92@gmail.com>
2024-02-27 09:47:36 +0100

    correction de mon data.ts

---
+++ src/app/shared/models/Category.ts
@@ -0,0 +1,6 @@
+export class Category {
+    id!: string;
+    name!: string;
+    icon_url!: string;
+
+}
--- src/app/shared/models/product.ts
+++ src/app/shared/models/product.ts
@@ -1,29 +1,12 @@
-export interface ICategoryImg {
-    src: string;
-}
-
-export interface IProductImg {
-    src: string;
-}
-
-export interface IProduct {
-    id: number;
-    title: string;
-    description: string;
-    dimension: string;
-    features: string[];
-    price: number;
-    img: IProductImg;
-    isAddToCart: boolean;
-    isAddToWishlist: boolean;
-}
-
-export type CategoryType = "Tous les produits" | "Salon" | "Cuisine" | "Salle à manger" | "Chambre" | "Bureau" | "Salle de bain" | "Textiles" | "Déco Murale" | "Luminaires" | "Accessoires";
-
-export interface ICategory {
-    id: number;
-    name: string;
-    subcategories: CategoryType[];
-    img: ICategoryImg;
-    products?: IProduct[];
+export class Product {
+  id!: string;
+  title!: string;
+  description!: string;
+  dimension!: string;
+  features!: string[];
+  price!: number;
+  img_url!: string ;
+  subCategory_id!: string;
+  // isAddToCart: boolean;
+  // isAddToWishlist: boolean;
 }
