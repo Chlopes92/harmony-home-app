66ad860502c202e354da5152c45ac76b0d350812
Christelle <christelle.lopes92@gmail.com>
2024-04-18 21:48:22 +0200

    affichage produit by id

--- src/app/shared/constants/urls.ts
+++ src/app/shared/constants/urls.ts
@@ -2,4 +2,5 @@ const BASE_URL = 'http://localhost:8080';
 
 export const CATEGORY_URL = `${BASE_URL}/api/category/`;
 export const SUB_CATEGORY_URL = `${BASE_URL}/api/sub-category/`;
-export const  PRODUCTS_URL = `${BASE_URL}/api/product/`;
+export const  PRODUCTS_URL = `${BASE_URL}/api/product/by-sub-category/`;
+export const PRODUCT_BY_ID_URL = `${BASE_URL}/api/product/`;
