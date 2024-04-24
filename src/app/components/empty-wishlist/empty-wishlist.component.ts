f5a29e8a217d1dfe5e60d593eb02aeca5f2eed76
Christelle <christelle.lopes92@gmail.com>
2024-04-24 13:28:31 +0200

    panier vide

--- src/app/components/empty-wishlist/empty-wishlist.component.ts
+++ src/app/components/empty-wishlist/empty-wishlist.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-empty-wishlist',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './empty-wishlist.component.html',
   styleUrl: './empty-wishlist.component.css'
 })
