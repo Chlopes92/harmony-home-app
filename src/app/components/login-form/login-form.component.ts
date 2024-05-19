575513936f7f90ea415b0505305a72acebd1a5c9
Christelle <christelle.lopes92@gmail.com>
2024-05-19 23:19:52 +0200

    correction ajout à la wishlist si login

--- src/app/components/login-form/login-form.component.ts
+++ src/app/components/login-form/login-form.component.ts
@@ -1,47 +1,63 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Router, RouterLink } from '@angular/router';
 import { UserService } from '../../services/user/user.service';
+import { WishlistService } from '../../services/wishlist/wishlist.service'; // Importez le service de wishlist
+import { Product } from '../../shared/models/Product'; // Assurez-vous d'importer le modèle Product
 
 @Component({
   selector: 'app-login-form',
   standalone: true,
   imports: [ReactiveFormsModule, RouterLink],
   templateUrl: './login-form.component.html',
-  styleUrl: './login-form.component.css'
+  styleUrls: ['./login-form.component.css']
 })
-export class LoginFormComponent {
+export class LoginFormComponent implements OnInit {
   loginForm!: FormGroup;
-  validationError: string [] = [];
+  validationError: string[] = [];
 
-  constructor(private formBuilder: FormBuilder, private router: Router, private userService: UserService){}
+  constructor(
+    private formBuilder: FormBuilder,
+    private router: Router,
+    private userService: UserService,
+    private wishlistService: WishlistService // Injectez le service de wishlist
+  ) {}
 
-  ngOnInit(){
-
+  ngOnInit() {
     this.loginForm = this.formBuilder.group({
-      email: ['', [Validators.required,]],
+      email: ['', [Validators.required]],
       password: ['', [Validators.required]],
     });
-
   }
 
-  onLogin(){
-    if(this.loginForm.valid){
+  onLogin() {
+    if (this.loginForm.valid) {
       const { email, password } = this.loginForm.value;
       this.userService.login(email, password).subscribe({
         next: ({ token, user }) => {
           localStorage.setItem('token', token);
-          // console.log('Connexion de: ', user);
-          
-          // Obtenir l'URL sauvegardée
+
+          // Obtenez l'URL sauvegardée
           const redirectUrl = localStorage.getItem('redirectUrl') || '/user-space';
           this.router.navigate([redirectUrl]);
-          localStorage.removeItem('redirectUrl'); // Nettoyer après la redirection
+          localStorage.removeItem('redirectUrl'); // Nettoyez après la redirection
+
+          // Vérifiez si un produit est en attente d'ajout à la liste de souhaits
+          this.checkPendingWishlistProduct();
         },
         error: error => {
           alert('Erreur lors de la connexion');
         }
       });
+    }
+  }
+
+  private checkPendingWishlistProduct(): void {
+    const pendingProduct = localStorage.getItem('pendingWishlistProduct');
+    if (pendingProduct) {
+      const product: Product = JSON.parse(pendingProduct);
+      this.wishlistService.addToWishlist(product);
+      localStorage.removeItem('pendingWishlistProduct');
     }
   }
 }
