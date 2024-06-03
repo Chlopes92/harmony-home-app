006051fa2db7c4a63734fbef4215257a2423b3a9
Christelle <christelle.lopes92@gmail.com>
2024-06-03 16:03:33 +0200

    style stepper formulaire de payement

--- src/app/pages/delivery-form/delivery-form.component.ts
+++ src/app/pages/delivery-form/delivery-form.component.ts
@@ -1,53 +1,59 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Router, RouterLink } from '@angular/router';
+import { Cart } from '../../shared/models/Cart';
+import { CartService } from '../../services/cart/cart.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-delivery-form',
   standalone: true,
-  imports: [ReactiveFormsModule, RouterLink],
+  imports: [CommonModule, RouterLink, ReactiveFormsModule],
   templateUrl: './delivery-form.component.html',
-  styleUrl: './delivery-form.component.css'
+  styleUrls: ['./delivery-form.component.css']
 })
-export class DeliveryFormComponent {
+export class DeliveryFormComponent implements OnInit {
+  cart!: Cart;
   deliveryForm!: FormGroup;
-  validationError: string [] = [];
+  validationError: string[] = [];
 
-  constructor(private formBuilder: FormBuilder, private router: Router){}
+  constructor(private cartService: CartService, private formBuilder: FormBuilder, private router: Router) {}
 
-  ngOnInit(){
+  ngOnInit() {
+    this.cartService.getCartObservable().subscribe((cart) => {
+      this.cart = cart;
+    });
+
     this.initDeliveryForm();
   }
 
-  initDeliveryForm(){
+  initDeliveryForm() {
     this.deliveryForm = this.formBuilder.group({
       delivery_name: [null, [Validators.required]],
       delivery_firstname: [null, [Validators.required]],
       delivery_country: [null, [Validators.required]],
       delivery_street: [null, [Validators.required]],
-      delivery_floor: [null, [Validators.required]],
+      delivery_floor: [null],
       delivery_postcode: [null, [Validators.required]],
       delivery_city: [null, [Validators.required]],
       delivery_phone: [null, [Validators.required]],
     });
   }
 
-  onDelivery(){
+  onDelivery() {
     this.validationError = [];
     console.log(this.deliveryForm.value);
 
-    if(this.deliveryForm.invalid){
-      Object.keys(this.deliveryForm.controls).forEach((input)=>{
+    if (this.deliveryForm.invalid) {
+      Object.keys(this.deliveryForm.controls).forEach((input) => {
         const currentInput = this.deliveryForm.get(input);
-        if(currentInput && currentInput.status === "INVALID"){
+        if (currentInput && currentInput.status === "INVALID") {
           this.validationError.push(input);
         }
-        // console.log(input,currentInput);
-      })
-      console.log(this.validationError)
-    }else{
+      });
+      console.log(this.validationError);
+    } else {
       this.router.navigate(['/payment-form']);
     }
   }
-
 }
