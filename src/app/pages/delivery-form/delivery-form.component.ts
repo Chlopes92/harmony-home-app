bdcf4abe9059b35cfd4b7414ea06cc9a8f0d2181
Christelle <christelle.lopes92@gmail.com>
2024-06-13 11:37:23 +0200

    correction du formulaire de livraison

--- src/app/pages/delivery-form/delivery-form.component.ts
+++ src/app/pages/delivery-form/delivery-form.component.ts
@@ -4,6 +4,7 @@ import { Router, RouterLink } from '@angular/router';
 import { Cart } from '../../shared/models/Cart';
 import { CartService } from '../../services/cart/cart.service';
 import { CommonModule } from '@angular/common';
+import { Observable, map, startWith } from 'rxjs';
 
 @Component({
   selector: 'app-delivery-form',
@@ -16,6 +17,9 @@ export class DeliveryFormComponent implements OnInit {
   cart!: Cart;
   deliveryForm!: FormGroup;
   validationError: string[] = [];
+  submitted = false; // Variable to track form submission
+  countries: string[] = ["France", "Belgique", "Suisse", "Canada", "États-Unis", "Allemagne", "Royaume-Uni"];
+  filteredCountries!: Observable<string[]>;
 
   constructor(private cartService: CartService, private formBuilder: FormBuilder, private router: Router) {}
 
@@ -25,25 +29,34 @@ export class DeliveryFormComponent implements OnInit {
     });
 
     this.initDeliveryForm();
+
+    this.filteredCountries = this.deliveryForm.controls['delivery_country'].valueChanges.pipe(
+      startWith(''),
+      map(value => this._filterCountries(value))
+    );
+  }
+
+  private _filterCountries(value: string): string[] {
+    const filterValue = value.toLowerCase();
+    return this.countries.filter(country => country.toLowerCase().includes(filterValue));
   }
 
   initDeliveryForm() {
     this.deliveryForm = this.formBuilder.group({
-      delivery_name: [null, [Validators.required]],
-      delivery_firstname: [null, [Validators.required]],
+      delivery_name: [null, [Validators.required, Validators.pattern("^[A-Za-zÀ-ÖØ-öø-ÿ '-]+$")]],
+      delivery_firstname: [null, [Validators.required, Validators.pattern("^[A-Za-zÀ-ÖØ-öø-ÿ '-]+$")]],
       delivery_country: [null, [Validators.required]],
-      delivery_street: [null, [Validators.required]],
+      delivery_street: [null, [Validators.required, Validators.pattern("^[A-Za-zÀ-ÖØ-öø-ÿ0-9 ,.'-]+$")]],
       delivery_floor: [null],
-      delivery_postcode: [null, [Validators.required]],
-      delivery_city: [null, [Validators.required]],
-      delivery_phone: [null, [Validators.required]],
+      delivery_postcode: [null, [Validators.required, Validators.pattern("^[0-9]{5}$")]],
+      delivery_city: [null, [Validators.required, Validators.pattern("^[A-Za-zÀ-ÖØ-öø-ÿ '-]+$")]],
+      delivery_phone: [null, [Validators.required, Validators.pattern("^[0-9]{10}$")]],
     });
   }
 
   onDelivery() {
+    this.submitted = true; // Mark form as submitted
     this.validationError = [];
-    console.log(this.deliveryForm.value);
-
     if (this.deliveryForm.invalid) {
       Object.keys(this.deliveryForm.controls).forEach((input) => {
         const currentInput = this.deliveryForm.get(input);
@@ -51,7 +64,6 @@ export class DeliveryFormComponent implements OnInit {
           this.validationError.push(input);
         }
       });
-      console.log(this.validationError);
     } else {
       this.router.navigate(['/payment-form']);
     }
