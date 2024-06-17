09886a1e1d6beadfc26aab4cc93996a7d775c24b
Christelle <christelle.lopes92@gmail.com>
2024-06-17 12:21:47 +0200

    avancement sur le style du formulaire de payement

--- src/app/pages/payment-form/payment-form.component.ts
+++ src/app/pages/payment-form/payment-form.component.ts
@@ -1,48 +1,57 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Router, RouterLink } from '@angular/router';
+import { CartService } from '../../services/cart/cart.service';
+import { Cart } from '../../shared/models/Cart';
+import { StripePaymentElementComponent } from 'ngx-stripe';
 
 @Component({
   selector: 'app-payment-form',
   standalone: true,
-  imports: [ReactiveFormsModule, RouterLink],
+  imports: [ReactiveFormsModule, RouterLink, StripePaymentElementComponent ], 
   templateUrl: './payment-form.component.html',
   styleUrl: './payment-form.component.css'
 })
 export class PaymentFormComponent {
   paymentForm!: FormGroup;
   validationError: string [] = [];
+  cart!: Cart;
 
-  constructor(private formBuilder: FormBuilder, private router: Router){}
+  constructor(private formBuilder: FormBuilder, private router: Router, private cartService: CartService) {} // Injection du service d'adresse
 
-  ngOnInit(){
-    this.initPaymenForm();
+  ngOnInit() {
+    this.initPaymentForm();
+
+    this.cartService.getCartObservable().subscribe((cart) => {
+      this.cart = cart;
+    });
+
   }
 
-  initPaymenForm(){
+  initPaymentForm() {
     this.paymentForm = this.formBuilder.group({
-      card_number: ['', [Validators.required]],
-      card_expiration: ['', [Validators.required]],
-      card_cvv: ['', [Validators.required]],
-      card_name: ['', [Validators.required]]
+      card_number: ['', [Validators.required, Validators.pattern('^[0-9]{16}$')]],
+      card_expiration: ['', [Validators.required, Validators.pattern('^(0[1-9]|1[0-2])\/?([0-9]{2})$')]],
+      card_cvv: ['', [Validators.required, Validators.pattern('^[0-9]{3,4}$')]],
+      card_name: ['', [Validators.required, Validators.pattern("^[A-Za-zÀ-ÖØ-öø-ÿ '-]+$")]]
     });
   }
 
-  onPay(){
+  onPay() {
     this.validationError = [];
     console.log(this.paymentForm.value);
 
-    if(this.paymentForm.invalid){
-      Object.keys(this.paymentForm.controls).forEach((input)=>{
+    if (this.paymentForm.invalid) {
+      Object.keys(this.paymentForm.controls).forEach((input) => {
         const currentInput = this.paymentForm.get(input);
-        if(currentInput && currentInput.status === "INVALID"){
+        if (currentInput && currentInput.status === "INVALID") {
           this.validationError.push(input);
         }
-        // console.log(input,currentInput);
-      })
-      console.log(this.validationError)
-    }else{
-      this.router.navigate(['/payment-form']);
+      });
+      console.log(this.validationError);
+    } else {
+      // Process payment
+      this.router.navigate(['/summary']);
     }
   }
 
