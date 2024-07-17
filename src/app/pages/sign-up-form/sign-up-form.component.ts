5f799c7d6986c22037dd2e1eb15890518c120c18
Christelle <christelle.lopes92@gmail.com>
2024-07-17 21:43:36 +0200

    correction style + arrangement pour le responsive

--- src/app/pages/sign-up-form/sign-up-form.component.ts
+++ src/app/pages/sign-up-form/sign-up-form.component.ts
@@ -2,60 +2,66 @@ import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Router, RouterLink } from '@angular/router';
 import { UserService } from '../../services/user/user.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-sign-up-form',
   standalone: true,
-  imports: [ReactiveFormsModule, RouterLink],
+  imports: [ReactiveFormsModule, RouterLink, CommonModule],
   templateUrl: './sign-up-form.component.html',
   styleUrl: './sign-up-form.component.css'
 })
 export class SignUpFormComponent {
-  isCreated: boolean = false; 
   signUpForm!: FormGroup;
-  validationError: string [] = [];
+  submitted = false;
+  hidePassword = true;
 
-  constructor(private formBuilder: FormBuilder, private router: Router, private userService: UserService){}
+  constructor(private formBuilder: FormBuilder, private router: Router, private userService: UserService) {}
 
   ngOnInit() {
     this.initSignUpForm();
   }
 
-  initSignUpForm(){
+  initSignUpForm() {
     this.signUpForm = this.formBuilder.group({
-      firstname: ['', [Validators.required]],
-      name: ['', [Validators.required]],
-      email: ['', [Validators.required]],
-      password: ['', [Validators.required]],
-      phone: ['', [Validators.required]],
+      firstname: ['', [Validators.required, Validators.pattern(/^[A-Za-zÀ-ÖØ-öø-ÿ\-']+$/)]],
+      name: ['', [Validators.required, Validators.pattern(/^[A-Za-zÀ-ÖØ-öø-ÿ\-']+$/)]],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [
+        Validators.required,
+        Validators.pattern(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_]).{12,}$/)
+      ]],
+      phone: ['', [Validators.required, Validators.pattern(/^\+?[0-9]{1,4}?[-.\s]?(\(?\d{1,3}?\))?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}$/)]],
     });
   }
 
-  onSignUp() {
-  if (this.signUpForm.invalid) {
-    console.log('Formulaire invalide', this.signUpForm.errors);
-    alert('Erreur lors de la création du compte');
-    return; // Arrête l'exécution si le formulaire est invalide
+  togglePasswordVisibility() {
+    this.hidePassword = !this.hidePassword;
   }
 
-  const formData = this.signUpForm.value;
-  this.signUp(formData.name, formData.firstname, formData.email, formData.password, formData.phone);
-}
+  get f() { return this.signUpForm.controls; }
 
-signUp(name: string, firstname: string, email: string, password: string, phone: string) {
-  this.userService.signup(name, firstname, email, password, phone).subscribe({
-    next: (data) => {
-      console.log("Utilisateur créé avec succès", data);
-      this.isCreated = true;
-      alert('Compte crée avec succès');
+  onSignUp() {
+    this.submitted = true;
 
-      // const redirectUrl = localStorage.getItem('/login');
-      //   this.router.navigate([redirectUrl]);
-        // localStorage.removeItem('redirectUrl'); // Nettoyer après la redirection
-    },
-    error: (error) => {
-      console.error("Erreur lors de la création de l'utilisateur", error);
+    if (this.signUpForm.invalid) {
+      return;
     }
-  });
-}
+
+    const formData = this.signUpForm.value;
+    this.signUp(formData.name, formData.firstname, formData.email, formData.password, formData.phone);
+  }
+
+  signUp(name: string, firstname: string, email: string, password: string, phone: string) {
+    this.userService.signup(name, firstname, email, password, phone).subscribe({
+      next: (data) => {
+        console.log("Utilisateur créé avec succès", data);
+        alert('Compte créé avec succès');
+        this.router.navigate(['/login']);
+      },
+      error: (error) => {
+        console.error("Erreur lors de la création de l'utilisateur", error);
+      }
+    });
+  }
 }
